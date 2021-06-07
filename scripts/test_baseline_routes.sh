408120fb22ce52c621423615c1b61d63b85fb80f
Ruaidhri <deveryr2@gmail.com>
2021-06-06 18:54:59 -0700

    Add host to routes baseline test

--- scripts/test_baseline_routes.sh
+++ scripts/test_baseline_routes.sh
@@ -43,7 +43,7 @@ echo "---"
 curl http://localhost:8000/example/advanced-settings
 echo
 echo "---"
-curl -X POST -H "Accept: application/x.treetop-html-template+xml" :8000/example/form
+curl -X POST -H "Accept: application/x.treetop-html-template+xml" http://localhost:8000/example/form
 echo
 echo "---- Feched example page successfully ---"
 
