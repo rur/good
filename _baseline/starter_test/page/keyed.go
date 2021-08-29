0e412138166140daf4f143d396e3133691888d79
Ruaidhri <deveryr2@gmail.com>
2021-08-28 17:14:30 -0700

    Update starter command baseline

---
+++ _baseline/starter_test/page/keyed.go
@@ -0,0 +1,6 @@
+package page
+
+var KeyedTemplates = map[string]string{
+	"::empty::":       "",
+	"::placeholder::": "<h1>Placeholder</h1> <p>Please run <code>go generate github.com/rur/good/baseline/starter_test/...</code></p>",
+}
