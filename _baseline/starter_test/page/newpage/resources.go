7cfe5ac3ce963f921205ab6d05380ba4ac77a8a2
Ruaidhri Devery <deveryr2@gmail.com>
2023-02-13 09:28:51 -0800

    Resource caching template change fix baselines

--- _baseline/starter_test/page/newpage/resources.go
+++ _baseline/starter_test/page/newpage/resources.go
@@ -1,9 +1,8 @@
 package newpage
 
 import (
+	"context"
 	"net/http"
-	"sync"
-	"time"
 
 	"github.com/rur/good/baseline/starter_test/page"
 	"github.com/rur/good/baseline/starter_test/site"
@@ -15,11 +14,12 @@ type resources struct {
 	user site.User
 	// EDITME: add request specific resources for this page
 }
+type handlerWithResources func(*resources, *site.Env, treetop.Response, *http.Request) interface{}
 
 // loadResources constructs a resources struct for use by the request handlers of this page
 //
 // If an error or failure occurs, this function is responsible for responding to the client using the ResponseWriter.
-func loadResources(env *site.Env, w http.ResponseWriter, req *http.Request) (rsc *resources, ok bool) {
+func loadResources(rspID uint32, env *site.Env, w http.ResponseWriter, req *http.Request) (rsc *resources, ok bool) {
 	// EDITME: setup your handler resources here
 	ok = true
 	rsc = &resources{
@@ -31,71 +31,32 @@ func loadResources(env *site.Env, w http.ResponseWriter, req *http.Request) (rsc
 	return
 }
 
-// teardownResources happens after the response has been written for the request,
+// teardownResources happens after the root handler returns
 // this hook exists in case any special teardown is needed for your resources instance
-func teardownResources(rsc *resources, env *site.Env) {
+func teardownResources(rspID uint32, rsc *resources, env *site.Env) {
 	// EDITME: Add teardown logic for your resources
 }
 
-//
-// resource caching mechanics
-//
+type localCxtKey string
 
-var (
-	rscCache     map[uint32]*resources
-	rscCacheLock sync.RWMutex
-)
+const pageRscCxtKey = localCxtKey("resource_key")
 
-type handlerWithResources func(*resources, *site.Env, treetop.Response, *http.Request) interface{}
-
-// bindResources is middleware with memoization which loads newpage page resources for local request handlers
+// bindResources uses the request context to cache request scoped resources
 func bindResources(f handlerWithResources) page.ViewHandlerWithEnv {
 	return func(env *site.Env, rsp treetop.Response, req *http.Request) interface{} {
-		key := rsp.ResponseID()
-		rscCacheLock.RLock()
-		rsc, ok := rscCache[key]
-		rscCacheLock.RUnlock()
-		if ok {
+		var rsc *resources
+		if rsc, ok := req.Context().Value(pageRscCxtKey).(*resources); ok {
 			return f(rsc, env, rsp, req)
 		}
 		// expensive path
-		rsc, ok = loadResources(env, rsp, req)
+		rsc, ok := loadResources(rsp.ResponseID(), env, rsp, req)
 		if !ok {
 			// an error ocurred, abort
 			return nil
 		}
-
-		// Cache resources for this response ID and setup a
-		// callback to tear it down when the response is finished
-		rscCacheLock.Lock()
-		rscCache[key] = rsc
-		rscCacheLock.Unlock()
-		go func() {
-			defer func() {
-				rscCacheLock.Lock()
-				rsc, ok := rscCache[key]
-				delete(rscCache, key)
-				rscCacheLock.Unlock() // release lock *before* attempting teardown
-				if ok {
-					teardownResources(rsc, env)
-				}
-			}()
-
-			// block until the response is concluded, defer will clear the rscCache entry
-			select {
-			case <-rsp.Context().Done():
-			case <-time.After(10 * time.Minute): // failsafe timeout to protect shared resources
-				if env.ErrorLog != nil {
-					env.ErrorLog.Println("failsafe request timeout expired after 10min for newpage page")
-				}
-				// since rsp Context is bound to the HTTP request, I assume that this is safe to do
-				http.Error(rsp, http.StatusText(http.StatusRequestTimeout), http.StatusRequestTimeout)
-			}
-		}()
-		return f(rsc, env, rsp, req)
+		cxt, cancel := context.WithCancel(context.WithValue(req.Context(), pageRscCxtKey, rsc))
+		defer teardownResources(rsp.ResponseID(), rsc, env)
+		defer cancel()
+		return f(rsc, env, rsp, req.WithContext(cxt))
 	}
-}
-
-func init() {
-	rscCache = make(map[uint32]*resources)
 }
